e86a5e331d74afa4eb528549cfb2e0315f2a9e98
Ilyumzhinov <Ilyumzhinov@gmail.com>
2018-07-09 17:58:20 -0400

    Comments added to the example

--- TensorFlowExample.py
+++ TensorFlowExample.py
@@ -1,3 +1,8 @@
+# To compile:
+## Open CommandPrompt or PowerShell
+## Navigate to the PythonTFSummerProject folder using the "cd" command
+## Type to compile: python TensorFlowExample.py
+
 import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
