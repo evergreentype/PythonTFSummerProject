7ca546b8564aae3b19edf9bc1631b0899c088504
Savr123 <Sangievv@mail.ru>
2018-07-13 23:34:18 +0300

    (Modules)

--- Savr.py
+++ Savr.py
@@ -22,3 +22,28 @@ v = ([1, 2, 3], [3, 2, 1])
 v[1][1]=5
 print(v[1][1])
 print(v)
+
+
+#**
+#Modules
+
+#A module is a file containing Python definitions and statements.
+#Module can contain other module
+#
+import artem
+import sys
+print('------------------------------------------------------------------------------')
+#dir() is a function that explore to us all defined names
+print(dir(sys))
+print('==============================================================================')
+print(dir())
+
+#**
+#Packages
+#Packages are a way of structuring Python’s module namespace by using “dotted module names”
+#For Example. import sound.effects.echo
+#--------------------------------------
+#Now what happens when the user writes from sound.effects import *?
+#it will add all moudles that contains in __all__ of __init__ file
+#but it also is a bad practice, because it can bring more other problems
+#--------------------------------------
