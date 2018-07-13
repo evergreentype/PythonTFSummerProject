962a939536faecd153cf72a93cec3d82e2bd881e
Savr123 <Sangievv@mail.ru>
2018-07-13 22:01:30 +0300

    Python Tutorial(a little part)
    Description of Tuple and lists

---
+++ Savr.py
@@ -0,0 +1,24 @@
+
+
+
+#list as matrix
+dec=[2,3,4,5,6]
+print(dec)
+#-----------------------
+
+
+#list processing
+dec=[x**2 for x in dec]
+#----------------------
+
+
+
+#tuples
+v = ([1, 2, 3], [3, 2, 1])
+#tuples can contain mutable objects
+#----------------------
+
+#deal with element of matrix 
+v[1][1]=5
+print(v[1][1])
+print(v)
