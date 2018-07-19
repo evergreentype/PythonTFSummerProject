e76ab2e22929c208767fedefd58e62eb998e7b97
Artem <ilumzhinov@gmail.com>
2018-07-19 16:07:15 -0400

    funClasses added

--- PythonP/funClasses.py
+++ PythonP/funClasses.py
@@ -2,18 +2,17 @@ import math
 
 class MathObject():
     def __init__(self):
-        pass
+        raise NotImplementedError("Must implement method find_value()")
 
     def find_value(self, *args):
         raise NotImplementedError("Must implement method find_value()")
 
 class Length(MathObject):
-    def find_value(self, *args):
-        if (len(args) == 1):
-            return float(args[0])
-        else:
-            raise Exception("Wrong amount of args")
+	def __init__(self):
+		self.__value = -1
 
-myval = Length()
-
-print(myval.find_value(1))
+	def find_value(self, *args):
+		if (len(args) == 1 and args[0] > 0):
+			return float(args[0])
+		else:
+			raise Exception("Wrong amount of args or value < 0")
--- PythonP/rectangularFigures.py
+++ PythonP/rectangularFigures.py
@@ -1,2 +1,2 @@
-import math
+import math, funClasses
 
