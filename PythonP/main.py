9f1520651d3531a06888f1ebe442b6427845ce92
Savr123 <Sangievv@mail.ru>
2018-07-20 02:06:37 +0300

    Area of Circle(half)

--- PythonP/main.py
+++ PythonP/main.py
@@ -1,5 +1,7 @@
 import funClasses
 import rectangularFigures
+import circleFig
+import baseClasses
 
 print('Choose figure')
 fig=('rectangle','square','cube','circle')
@@ -8,3 +10,8 @@ for x in range(1,5):
 	print(str(x)+'-'+fig[x-1])
 FigSelected=input()
 print(fig[int(FigSelected)-1]+' was choosen')
+if int(FigSelected)==4:
+	print('input radius')
+	radius=input()
+	figure=Area(radius)
+	CircleFig.findValue()
