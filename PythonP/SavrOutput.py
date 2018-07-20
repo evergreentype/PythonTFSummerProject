3799e1cbfa22b5906dd7b11755a2595261e6d0c3
Savr123 <Sangievv@mail.ru>
2018-07-20 23:01:34 +0300

    Area

--- PythonP/SavrOutput.py
+++ PythonP/SavrOutput.py
@@ -2,14 +2,31 @@ import circleFig
 import baseClasses
 
 print('Choose figure')
-fig=('rectangle','square','cube','circle')
+fig=('triangle','parallelogram','cube','circle')
 
 
 for x in range(1,5):
 	print(str(x)+'-'+fig[x-1])
-FigSelected=input()
+
+FigSelected=int(input())
 print(fig[int(FigSelected)-1]+' was choosen')
-print('input radius')
-radius=input()
-circleFig=circleFig.FigSelec()
-print(circleFig.find_value(FigSelected,radius))
+radius=0
+base=0
+height=0
+if FigSelected==4:
+	print('input radius ')
+	radius=int(input())
+elif FigSelected==1:
+	print('input height ')
+	height=int(input())
+	print('input base ')
+	base=int(input())
+elif FigSelected==2:
+	print('input height ')
+	height=int(input())
+	print('input base1 ')
+	base=int(input())
+	print('input base2')
+	base+=int(input())
+circleFig=circleFig.FigSelec(FigSelected,radius,base,height)
+print("{:.1f} cm^2".format(circleFig.find_value()))
--- PythonP/circleFig.py
+++ PythonP/circleFig.py
@@ -3,23 +3,48 @@ import math
 
 class FigSelec(baseClasses.MathObject):
 
-	def __init__(self):
-		pass
+	def __init__(self,FigSelected,rad,height,base):
+		self.rad=rad
+		self.FigSelected=FigSelected
+		self.height=height
+		self.base=base
 
-	def find_value(self,FigSelected,rad):
-		print('FigSele2323232c')	
-		return Area.find_value()
-		if FigSelected==4:
-			Area=circleFig.Area(rad)
-			print('12312')
-			return Area.find_value()
+	def find_value(self):
+		if self.FigSelected==4:
+			area=Area(self.FigSelected,self.rad,self.height,self.base)
+			return area.find_value()
+		elif self.FigSelected==1:
+			area=Area(self.FigSelected,self.rad,self.height,self.base)
+			return area.find_value()
+		elif self.FigSelected==2:
+			area=Area(self.FigSelected,self.rad,self.height,self.base)
+			return area.find_value()
 
 class Area(FigSelec):
 	"""compute Area of figures"""
-	def __init__(self,rad):
+	def __init__(self,FigSelected,rad,height,base):
 		self.rad=rad
+		self.height=height
+		self.base=base
+		self.FigSelected=FigSelected
 
-	def find_value(self):
-		print('asdFigSelec')
-		print(math.pi*(self.rad**2))
-		return math.pi*(self.rad**2)
+	def find_value(self, *args):
+		"""Controler method"""
+		if self.FigSelected==4:
+			return self.find_value4()
+		elif self.FigSelected==1:
+			return self.find_value1()
+		elif self.FigSelected==2:
+			return self.find_value2()
+		elif self.FigSelected==3:
+			pass
+
+	def find_value1(self):
+		return (self.height*self.base)/2
+
+	def find_value2(self):
+		return self.height*self.base
+	def find_value3(self):
+		pass
+	def find_value4(self):
+		return math.pi*(self.rad**2)	
