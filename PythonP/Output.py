96c3438c2f75cb8a19f75328ab7358e7e2ec5978
ArtemIl <ilumzhinov@gmail.com>
2018-07-28 19:29:57 -0400

    removed from Artem test

--- PythonP/Output.py
+++
@@ -1,9 +0,0 @@
-import classes
-import math
-
-
-print('input name of figure (circle,parallelogram, triangle)')
-name=input()
-figure=classes.Math_Obj()
-print(f'{figure.area(name):.2f} cm^2')
-print(f'{figure.perimetr(name):.2f} cm')
